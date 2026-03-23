acbeccbd4622548b099f0d0e2df2151850652d9f
jcapecci09 <jcapecci3@gmail.com>
2026-03-23 11:54:20 -0500

    finished ncbi_extract.py

--- ncbi_extract.py
+++ ncbi_extract.py
@@ -4,39 +4,58 @@
 
 from Bio import Entrez
 from Bio import SeqIO
+import argparse
+import sys
 
 
-# Open example file and read in terms
-with open('example_input.txt', 'r') as f:
-    email = f.readline()
-    search_term = f.readline()
-    seq_count = f.readline()
-    start_date = f.readline()
-    end_date = f.readline()
+def main():
+    # Set up parser for command line interface
+    parser = argparse.ArgumentParser(description='Pulls fasta sequences from NCBI')
+    parser.add_argument('-i', '--input', help='input file', required=True)
+    parser.add_argument('-o', '--output', help='output file', required=True)
 
-# Set email
-Entrez.email = email
+    # Define infile and outfile
+    args = parser.parse_args(sys.argv[1:])
+    infile = args.input
+    outfile = args.output
 
-# Search for proteins
-searchResultHandle = Entrez.esearch(db = "protein", term = search_term, 
-                                    retmax = seq_count, idtype = "protein", 
-                                    datetype = "pdat", mindate = start_date, 
-                                    maxdate = end_date)
 
-# Find id lists
-searchResult = Entrez.read(searchResultHandle)           
-ids = searchResult["IdList"]                            
+    # Open example file and read in terms
+    with open(infile, 'r') as f:
+        email = f.readline().strip()
+        search_term = f.readline().strip()
+        seq_count = int(f.readline().strip())
+        start_date = f.readline().strip()
+        end_date = f.readline().strip()
 
-# Find fastas based on ids
-handle = Entrez.efetch(db="protein", id=ids, rettype="fasta", retmode="text") 
-records = SeqIO.parse(handle, "fasta")
+    # Set email
+    Entrez.email = email
 
-# deduplicates sequences
-seen = set()
-deduplicated = []
+    # Search for proteins
+    searchResultHandle = Entrez.esearch(db = "protein", term = search_term, 
+                                        retmax = seq_count, datetype = "pdat", 
+                                        mindate = start_date, maxdate = end_date)
 
-for record in records:
-    seq = str(record.seq)
-    if seq not in seen:
-        deduplicated.append(record)
-        seen.add(seq)
+    # Find id lists
+    searchResult = Entrez.read(searchResultHandle)           
+    ids = searchResult["IdList"]                         
+
+    # Find fastas based on ids
+    handle = Entrez.efetch(db="protein", id=ids, rettype="fasta", retmode="text") 
+    records = SeqIO.parse(handle, "fasta")
+
+    # deduplicates sequences
+    seen = set()
+    deduplicated = []
+
+    for record in records:
+        seq = str(record.seq)
+        if seq not in seen:
+            deduplicated.append(record)
+            seen.add(seq)
+    
+    with open(outfile, 'w') as f:
+        SeqIO.write(deduplicated, f, "fasta")
+
+if __name__  == '__main__':
+    main()
